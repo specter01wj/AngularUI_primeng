95c8a61928f6e9393461ef50ff196f8421a64544
Jin Wang <specter01wj@gmail.com>
2024-01-26 17:23:58 -0500

    v2

--- mylab/AutoComplete/primeng-autocomplete/src/app/app.module.ts
+++ mylab/AutoComplete/primeng-autocomplete/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { SharedPrimeNGModule } from './modules/primeng/primeng.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AutocompleteComponent } from './autocomplete/autocomplete.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     SharedPrimeNGModule,
     AppRoutingModule
   ],
--- mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
+++ mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutoCompleteCompleteEvent } from '../common/models/autocomplete';
 
 @Component({
   selector: 'app-autocomplete',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './autocomplete.component.scss'
 })
 export class AutocompleteComponent {
+
+  items: any[] | undefined;
+
+  selectedItem: any;
+
+  suggestions: any[] | undefined;
+
+  search(event: AutoCompleteCompleteEvent) {
+      this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+  }
+
+
 
 }
