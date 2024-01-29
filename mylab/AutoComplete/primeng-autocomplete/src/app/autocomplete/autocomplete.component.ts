b06ce5c7efe941decc5e3993dc4be4bd6cd27461
Jin Wang <specter01wj@gmail.com>
2024-01-29 00:34:37 -0500

    v2

--- mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
+++ mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
@@ -12,6 +12,7 @@ import { FilterService, SelectItemGroup } from 'primeng/api';
 export class AutocompleteComponent implements OnInit {
   items: any[] | undefined;
   selectedItem: any;
+  filteredItems: any[] | undefined;
   selectedCity: any;
   suggestions: any[] | undefined;
   countries: any[] | undefined;
@@ -37,6 +38,7 @@ export class AutocompleteComponent implements OnInit {
       this.formGroup = new FormGroup({
           selectedCountry: new FormControl<object | null>(null)
       });
+      this.initFilterItems();
   }
 
   search(event: AutoCompleteCompleteEvent) {
@@ -105,6 +107,28 @@ export class AutocompleteComponent implements OnInit {
             ]
         }
     ];
+  }
+
+  filterItems(event: AutoCompleteCompleteEvent) {
+      //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+      let filtered: any[] = [];
+      let query = event.query;
+
+      for (let i = 0; i < (this.items as any[]).length; i++) {
+          let item = (this.items as any[])[i];
+          if (item.label.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+              filtered.push(item);
+          }
+      }
+
+      this.filteredItems = filtered;
+  }
+
+  initFilterItems() {
+    this.items = [];
+    for (let i = 0; i < 10000; i++) {
+        this.items.push({ label: 'Item ' + i, value: 'Item ' + i });
+    }
   }
 
 }
