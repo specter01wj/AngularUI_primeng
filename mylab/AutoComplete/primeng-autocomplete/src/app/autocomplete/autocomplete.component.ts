81c93665c00cbb2a2d7c1e5ada851b260fef6b6a
Jin Wang <specter01wj@gmail.com>
2024-01-29 00:30:21 -0500

    v1

--- mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
+++ mylab/AutoComplete/primeng-autocomplete/src/app/autocomplete/autocomplete.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AutoCompleteCompleteEvent } from '../common/models/autocomplete';
 import { CountryService } from '../service/country-service.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { FilterService, SelectItemGroup } from 'primeng/api';
 
 @Component({
   selector: 'app-autocomplete',
@@ -11,6 +12,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AutocompleteComponent implements OnInit {
   items: any[] | undefined;
   selectedItem: any;
+  selectedCity: any;
   suggestions: any[] | undefined;
   countries: any[] | undefined;
   selectedCountryAdvanced: any[] | undefined;
@@ -18,10 +20,16 @@ export class AutocompleteComponent implements OnInit {
   formGroup: FormGroup | undefined;
   selectedCountry: any;
   filteredCountries: any[] | undefined;
+  filteredGroups: any[] | undefined;
+  groupedCities: SelectItemGroup[] | undefined;
 
-  constructor(private _countryService: CountryService) {}
+  constructor(
+    private _countryService: CountryService,
+    private filterService: FilterService
+  ) {}
 
   ngOnInit() {
+      this.InitGroupedCities();
       this._countryService.getCountries().then((countries) => {
           this.countries = countries;
       });
@@ -47,6 +55,56 @@ export class AutocompleteComponent implements OnInit {
       }
 
       this.filteredCountries = filtered;
+  }
+
+  filterGroupedCity(event: AutoCompleteCompleteEvent) {
+      let query = event.query;
+      let filteredGroups = [];
+
+      for (let optgroup of this.groupedCities) {
+          let filteredSubOptions = this.filterService.filter(optgroup.items, ['label'], query, "contains");
+          if (filteredSubOptions && filteredSubOptions.length) {
+              filteredGroups.push({
+                  label: optgroup.label,
+                  value: optgroup.value,
+                  items: filteredSubOptions
+              });
+          }
+      }
+
+      this.filteredGroups = filteredGroups;
+  }
+
+  InitGroupedCities() {
+    this.groupedCities = [
+        {
+            label: 'Germany', value: 'de',
+            items: [
+                { label: 'Berlin', value: 'Berlin' },
+                { label: 'Frankfurt', value: 'Frankfurt' },
+                { label: 'Hamburg', value: 'Hamburg' },
+                { label: 'Munich', value: 'Munich' }
+            ]
+        },
+        {
+            label: 'USA', value: 'us',
+            items: [
+                { label: 'Chicago', value: 'Chicago' },
+                { label: 'Los Angeles', value: 'Los Angeles' },
+                { label: 'New York', value: 'New York' },
+                { label: 'San Francisco', value: 'San Francisco' }
+            ]
+        },
+        {
+            label: 'Japan', value: 'jp',
+            items: [
+                { label: 'Kyoto', value: 'Kyoto' },
+                { label: 'Osaka', value: 'Osaka' },
+                { label: 'Tokyo', value: 'Tokyo' },
+                { label: 'Yokohama', value: 'Yokohama' }
+            ]
+        }
+    ];
   }
 
 }
