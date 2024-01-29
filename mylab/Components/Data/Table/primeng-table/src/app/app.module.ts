c882d3648973c620929ee8be2be136489a57bd5f
Jin Wang <specter01wj@gmail.com>
2024-01-29 10:29:26 -0500

    v2

--- mylab/Components/Data/Table/primeng-table/src/app/app.module.ts
+++ mylab/Components/Data/Table/primeng-table/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedPrimeNGModule } from './modules/primeng/primeng.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedPrimeNGModule,
     AppRoutingModule
   ],
   providers: [],
