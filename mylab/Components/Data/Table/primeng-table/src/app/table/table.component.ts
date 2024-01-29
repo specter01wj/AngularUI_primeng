f1365bf7e56c777ae73105a1f096b6535e385d1c
Jin Wang <specter01wj@gmail.com>
2024-01-29 10:52:16 -0500

    v3

--- mylab/Components/Data/Table/primeng-table/src/app/table/table.component.ts
+++ mylab/Components/Data/Table/primeng-table/src/app/table/table.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../common/models/table';
+import { ProductServiceService } from '../service/product-service.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+  products!: Product[];
+
+  constructor(private _productServiceService: ProductServiceService) {}
+
+  ngOnInit() {
+      this._productServiceService.getProductsMini().then((data) => {
+          this.products = data;
+      });
+  }
+
+
+
+
+
 
 }
