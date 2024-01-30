d7f034270b2774fe07c985bcfe2413f0cfc43623
Jin Wang <specter01wj@gmail.com>
2024-01-29 19:15:53 -0500

    v7

--- mylab/Components/Data/Table/primeng-table/src/app/components/template/template.component.ts
+++ mylab/Components/Data/Table/primeng-table/src/app/components/template/template.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product, Column } from '../../common/models/table';
+import { ProductServiceService } from '../../service/product-service.service';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrl: './template.component.scss'
 })
-export class TemplateComponent {
+export class TemplateComponent implements OnInit {
+  products!: Product[];
+
+  constructor(private _productServiceService: ProductServiceService) {}
+
+  ngOnInit() {
+      this._productServiceService.getProductsMini().then((data) => {
+          this.products = data;
+      });
+  }
+
+  getSeverity(status: string) {
+      switch (status) {
+          case 'INSTOCK':
+              return 'success';
+          case 'LOWSTOCK':
+              return 'warning';
+          case 'OUTOFSTOCK':
+              return 'danger';
+          default:
+              return null;
+      }
+  }
 
 }
